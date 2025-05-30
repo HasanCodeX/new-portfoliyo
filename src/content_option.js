35ec5a647b98430b96e6a4e776fbf94b21810fcc
Mehedi Hasan <dev.hasan10@gmail.com>
2025-05-31 01:57:20 +0600

    (feat) i upadate the banner image from imagebb

--- src/content_option.js
+++ src/content_option.js
@@ -13,7 +13,7 @@ const introdata = {
     },
     description: "Hi, I'm Mehedi Hasan — a passionate Frontend Developer based in Bangladesh. I specialize in building modern, responsive web applications using technologies like React, Tailwind CSS, and Next.js.",
     // your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
-     your_img_url: "https://i.ibb.co/1ttg1fmg/personal-1.jpg",
+     your_img_url: "https://i.ibb.co/ycp6551P/profile-all.jpg",
 
 
     
